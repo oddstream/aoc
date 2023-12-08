e8bc49ffeb858e1acb8277c7c28c2268c35d7a99
oddstream <oddstream@googlemail.com>
2023-12-08 16:04:59 +0000

    2023 day 8

--- 2023/lua/08.lua
+++ 2023/lua/08.lua
@@ -3,31 +3,38 @@
 local log = require 'log'
 
 ---@param filename string
----@return string, table
+---@return string, table[]table
 local function loadInput(filename)
-	-- TODO a better way of handling multi-sectioned input
-	local instructions = ''
+	local directions = ''
 	local map = {}
-	for line in io.lines(filename) do
-		if #line > 0 then
-			if #instructions == 0 and line:match'[RL]+' ~= nil then
-				instructions = line
-			else
-				-- %w (alphanumerics), for part 2 test
-				-- otherwise %u (upper case letters) will do
-				local start, left, right = line:match'(%w%w%w)[%W]+(%w%w%w)[%W]+(%w%w%w)'
-				if start ~= nil and left ~= nil and right ~= nil then
-					if map[start] ~= nil then
-						log.error('repeat %s\n', start)
-					end
-					map[start] = {L=left, R=right}
-				else
-					log.error('unknown line %s\n', line)
-				end
-			end
+	local file, err = io.open(filename, 'r')
+	if file == nil then
+		log.error('cannot open %s - %s\n', filename, err)
+	else
+		directions = file:read()	-- default is to read a line, skipping the EOL
+
+		local line = file:read()
+		while line == '' do
+			line = file:read()
 		end
+
+		while line ~= nil do
+			local start, left, right = line:match'(%w%w%w)[%W]+(%w%w%w)[%W]+(%w%w%w)'
+			if start ~= nil and left ~= nil and right ~= nil then
+				if map[start] ~= nil then
+					log.error('repeat %s\n', start)
+				end
+				map[start] = {L=left, R=right}
+			else
+				log.error('unknown line %s\n', line)
+			end
+
+			line = file:read()
+		end
+		assert(io.close(file))
 	end
-	return instructions, map
+
+	return directions, map
 end
 
 ---@param filename string
@@ -36,7 +43,7 @@ end
 local function partOne(filename, expected)
 	local result = 0
 
-	local ins, mp = loadInput(filename)
+	local dirs, mp = loadInput(filename)
 	-- "You feel like AAA is where you are now,
 	-- and you have to follow the left/right instructions until you reach ZZZ."
 	-- "feel like"?
@@ -44,7 +51,7 @@ local function partOne(filename, expected)
 	-- the first and last entries in the input, viz DGK and TLL
 	local pos = 'AAA'
 	repeat
-		for dir in ins:gmatch'.' do
+		for dir in dirs:gmatch'.' do
 			-- if mp[pos] == nil or mp[pos][dir] == nil then
 			-- 	log.error('no map entry for %s %s\n', pos, dir)
 			-- 	break
@@ -73,20 +80,32 @@ end
 local function partTwo(filename, expected)
 	local result
 
+	---@param a integer
+	---@param b integer
+	---@return integer
 	local function gcd(a, b)
 		-- Euclidean Algorithm
 		return a == 0 and b or gcd(b % a, a)
 	end
 
+	---@param a integer
+	---@param b integer
+	---@return integer
 	local function lcm(a, b)
 		return a * b / gcd(a, b)
 	end
 
+	---@param arr integer[]
+	---@return integer
 	local function lcm_array(arr)
-		return #arr == 1 and arr[1] or lcm(arr[1], lcm_array{table.unpack(arr, 2)})
+		if _VERSION == 'Lua 5.1' then
+			return #arr == 1 and arr[1] or lcm(arr[1], lcm_array{unpack(arr, 2)})
+		else
+			return #arr == 1 and arr[1] or lcm(arr[1], lcm_array{table.unpack(arr, 2)})
+		end
 	end
 
-	local ins, mp = loadInput(filename)
+	local dirs, mp = loadInput(filename)
 
 	-- brute force wasn't going to work (result is 11,283,670,395,017)
 	-- so found the LCM hint on the AOC solutions subreddit (thank you kind strangers)
@@ -103,7 +122,7 @@ local function partTwo(filename, expected)
 	for _, pos in ipairs(starts) do
 		local n = 0
 		repeat
-			for dir in ins:gmatch'.' do
+			for dir in dirs:gmatch'.' do
 				pos = mp[pos][dir]
 				n = n + 1
 			end
